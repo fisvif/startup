9e1ee1d66b6c638ba00fddc173b5637ff8c4a43e
fisvif <shutkovadim@mail.ru>
2021-01-05 18:58:09 +0200

    dorobka design

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -7,18 +7,36 @@ let firstSection = document.querySelector(".services");
     upScroll = document.querySelector(".up-scroll"),
     quote = document.querySelectorAll(".quote"),
     pointsBox = document.querySelectorAll(".points-box"),
-    numberQuote = 0;
+    numberQuote = 0,
+    filterWord = document.querySelectorAll(".filter-word");
 
 
     popup.onclick = popupOpen;
     ButtonGoForm.onclick= scrollBottom;
     window.onscroll = onScrolling;
     upScroll.onclick = scrollTop;
+    popupBox.onclick = popupCloase;
     quoteScroll();
 
 
 function popupOpen(){// dorobutu
-    popupBox.innerHTML = `<div class="ssss">Loading</div>`;
+    popupBox.style.width = "100%";
+    popupBox.style.height = "100%";
+    popupBox.style.zIndex = "5";
+    popupBox.innerHTML = `<div class="register">
+        <h4>Registration</h4>
+        <form>
+            <label><input type="text"></input></label>
+            <label><input type="text"></input></label>
+            <label><input type="text"></input></label>
+            <label><input type="submit"></input></label>
+        </form>
+    </div>`;
+}
+function popupCloase(){
+    popupBox.style.width = "0";
+    popupBox.style.height = "0";
+    popupBox.style.zIndex = "-1";
 }
 function scrollBottom(event){
     event.preventDefault();
@@ -56,16 +74,27 @@ function scrollTop(){
         },1)
     }
 }
-function quoteScroll(){// dorobutu
-    if(numberQuote == quotes.length){
+function quoteScroll(){
+    if(numberQuote == quote.length){
         numberQuote = 0;
     }
-    for(let i = 1; i < quotes.length; i++){
-        pointsBox.innerHTML = `<span></span>`;
-    }
-    quote.style.display = "block";
+    quote[numberQuote].style.display = "block";
     numberQuote++;
     setTimeout(()=>{
+        quote[numberQuote - 1].style.display = "none";
         quoteScroll();
-    },1000)
+    },1500)
+}
+function filter(){
+    if(){
+        
+    }else if(){
+
+    }else if(){
+
+    }else if(){
+
+    }else if(){
+
+    }
 }
